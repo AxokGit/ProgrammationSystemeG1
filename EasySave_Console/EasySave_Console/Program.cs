83158b9fc204135ebe0f8775d11932031834782e
AxokGit <louis.dumont@viacesi.fr>
2022-11-17 09:29:16 +0100

    First test of console application

--- EasySave_Console/EasySave_Console/Program.cs
+++ EasySave_Console/EasySave_Console/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string version = "1.0.0";
+        string? menuNumber;
+
+        Console.WriteLine("|------------------------------|");
+        Console.WriteLine("|---- EasySave Console Mode ---|");
+        Console.WriteLine("|------------------------------|");
+        Console.WriteLine("Version: " + version + "\n");
+        Console.WriteLine("Options :");
+        Console.WriteLine("1 - MENU - Save");
+        Console.WriteLine("2 - MENU - Backup");
+        Console.WriteLine("3 - MENU - Settings");
+        Console.WriteLine("\n");
+        Console.Write("Options (1-3):");
+        menuNumber = Console.ReadLine();
+        if (menuNumber != null && menuNumber == "1") {
+            Console.WriteLine("-- MENU - Save --");
+        
+        }
+
+
+
+
+
+
+        Console.ReadKey();
+    }
+}

2ffbc140743339643f1f3d037711a07ad7518f15
Maxime EY <maxime.ey@viacesi.fr>
2022-11-21 20:15:21 +0100

    Translation works

---
+++ EasySave_Console/EasySave_Console/LangHelper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Resources;
+using System.Reflection;
+using System.Globalization;
+
+namespace EasySave_Console
+{
+    public static class LangHelper
+    {
+        private static ResourceManager _rm;
+
+        static LangHelper()
+        {
+            _rm = new ResourceManager("EasySave_Console.Languages.strings", Assembly.GetExecutingAssembly());
+        }
+
+        public static string? GetString(string name)
+        {
+            return _rm.GetString(name);
+        }
+        public static void ChangeLanguage(string language)
+        {
+            var cultureInfo = new CultureInfo(language);
+
+            CultureInfo.CurrentCulture = cultureInfo;
+            CultureInfo.CurrentUICulture = cultureInfo;
+        }
+    }
+}

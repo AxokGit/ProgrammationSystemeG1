8ada780df35836b669596d7f23fee0f27f1ba9bf
Louis DUMONT <louis.dumont@viacesi.fr>
2022-11-23 14:26:49 +0100

    Optimization of LangHelper class

--- EasySave_Console/EasySave_Console/LangHelper.cs
+++ EasySave_Console/EasySave_Console/LangHelper.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Resources;
+﻿using System.Resources;
 using System.Reflection;
-using System.Globalization;
 
 namespace EasySave_Console
 {
@@ -11,21 +7,14 @@ namespace EasySave_Console
     {
         private static ResourceManager _rm;
 
-        static LangHelper()
-        {
-            _rm = new ResourceManager("EasySave_Console.Languages.strings", Assembly.GetExecutingAssembly());
-        }
-
         public static string? GetString(string name)
         {
             return _rm.GetString(name);
         }
+
         public static void ChangeLanguage(string language)
         {
-            var cultureInfo = new CultureInfo(language);
-
-            CultureInfo.CurrentCulture = cultureInfo;
-            CultureInfo.CurrentUICulture = cultureInfo;
+            _rm = new ResourceManager("EasySave_Console.Languages."+language, Assembly.GetExecutingAssembly());
         }
     }
 }

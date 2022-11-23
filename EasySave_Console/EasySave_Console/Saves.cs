6afcc965093aa57459944faa9245beb7e6d43091
Maxime EY <maxime.ey@viacesi.fr>
2022-11-23 16:42:13 +0100

    Starting saves

---
+++ EasySave_Console/EasySave_Console/Saves.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace EasySave_Console
+{
+    class Saves
+    {
+        public void ConvertObjectToString()
+        {
+            var serializedObject = JsonConvert.SerializeObject("test 2");
+        }
+
+        
+    }
+
+    public class Save
+    {
+        public string 
+    }
+}

b4094f979cf9953bbed562434e800bd0cd44ee3d
Louis DUMONT <louis.dumont@viacesi.fr>
2022-11-24 09:30:05 +0100

    Updating dev from Livrable_1

--- EasySave_Console/EasySave_Console/Controllers/MenuController.cs
+++ EasySave_Console/EasySave_Console/Controllers/MenuController.cs
@@ -8,44 +8,39 @@ namespace EasySave_Console.Controllers
     class MenuController
     {
         MenuView menuView = new MenuView();
-        LanguageMenuView languageMenuView = new LanguageMenuView();
 
         public MenuController()
         {
-            bool languageSelected = false;
-            while (!languageSelected)
-            {
-                menuView.ClearConsole();
-                menuView.OnBootMessage();
-                string languageOption = languageMenuView.PromptLanguageOption();
-                if (languageOption == "1")
-                {
-                    LangHelper.ChangeLanguage("fr");
-                    languageSelected = true;
-                }
-                else if (languageOption == "2")
-                { 
-                    LangHelper.ChangeLanguage("en");
-                    languageSelected = true;
-                }
-            }
+            new LanguageController();
 
             bool optionSelected = false;
             while (!optionSelected)
             {
                 menuView.ClearConsole();
-                menuView.PromptMainMenu();
                 string menuOption = menuView.PromptMainMenu();
                 if (menuOption == "1")
                 {
                     menuView.Print("TODO");
+                    menuView.Wait();
                 }
                 else if (menuOption == "2")
                 {
-                    BackupWorksEditView backupWorksEditView = new BackupWorksEditView();
-                    JsonHelper jsonHelper = new JsonHelper();
-                    List<BackupWork>? backupWorks = jsonHelper.ReadBackupWorkFromJson("C:/BackupWorks.json");
-                    backupWorksEditView.PromptEditBackupWorks(backupWorks);
+                    optionSelected = true;
+                    new BackupWorksEditController();
+                }
+                else if (menuOption == "3")
+                {
+                    menuView.Print("TODO");
+                    menuView.Wait();
+                }
+                else if (menuOption == "4")
+                {
+                    menuView.Print("TODO");
+                    menuView.Wait();
+                }
+                else if (menuOption == "5")
+                {
+                    Environment.Exit(0);
                 }
             }
         }

7a02a48893d4a33148407a1daa90e6c91d037d03
Maxime EY <maxime.ey@viacesi.fr>
2022-11-25 23:11:51 +0100

    Work in progress

---
+++ EasySave_Console/EasySave_Console/Models/Logs.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Text;
+
+namespace EasySave_Console.Models
+{
+    class Logs
+    {
+        public string? Name { get; set; }
+        public string? FileSource { get; set; }
+        public string? FileTarget { get; set; }
+        public string? DestPath { get; set; }
+        public int? FileSize { get; set; }
+        public int? FileTransferTime { get; set; }
+        public string? Time { get; set; }
+        
+        public void GenerateLog(string message)
+        {
+            string LogPath = ConfigurationManager.AppSettings["logPath"];
+
+            using(StreamWriter writer = new StreamWriter(LogPath, true))
+        }
+
+    }
+}

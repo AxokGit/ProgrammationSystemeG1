1d002287e782e3ebf1a6f8712dde61f281ab6736
Louis DUMONT <louis.dumont@viacesi.fr>
2022-11-26 10:46:04 +0100

    FormatFilePath transferred in FileHelper

---
+++ EasySave_Console/EasySave_Console/FileHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EasySave_Console
+{
+    class FileHelper
+    {
+        public string filepath_bw_config { get; set; } = @"%AppData%/BackupWorks.json";
+        public string FormatFilePath(string path)
+        {
+            return Environment.ExpandEnvironmentVariables(path);
+        }
+
+    }
+}

7e0430311cc41336cce76058d9e66d60e08344cf
Maxime EY <maxime.ey@viacesi.fr>
2022-11-26 17:33:53 +0100

    Deleting useless lines

--- EasySave_Console/EasySave_Console/Models/Logs.cs
+++ EasySave_Console/EasySave_Console/Models/Logs.cs
@@ -16,10 +16,8 @@ namespace EasySave_Console.Models
         public string? Time { get; set; }
         public static string GenerateLog(BackupWork BO)
         {
-            //BackupWork MyObjet = new BackupWork();
             string strResultJson = JsonConvert.SerializeObject(BO);
             File.WriteAllText(@"logs.json", strResultJson);
-            Console.WriteLine("Stored!");
 
             return string.Format("Logs information:\n\tName: {0}, Source foler: {1}, Destination folder: {2}, Type : {3}, Size: {4}, Transfer Time: {5}, Time: {6}",
                                    BO.Name, BO.SrcFolder, BO.DstFolder, BO.Type, BO.FileSize, BO.FileTransferTime, BO.Time);

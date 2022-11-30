38f43187da9b1d8f2976afb9c92b09048c54ec07
Maxime EY <maxime.ey@viacesi.fr>
2022-11-30 09:53:11 +0100

    Work started

---
+++ EasySave_Console/EasySave_Console/Views/ConvertExtensionView.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using EasySave_Console.Models;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EasySave_Console.Views
+{
+    class ConvertExtensionView
+    {
+        TableView tableView = new TableView(110);
+        public void PromptConversion()
+        {
+            Console.WriteLine($"{LangHelper.GetString("choose_log")}");
+        }
+    }
+}

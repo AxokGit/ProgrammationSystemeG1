4c6def9cc34f559b965c1fd926814070342325e1
Louis DUMONT <louis.dumont@viacesi.fr>
2022-12-06 15:03:24 +0100

    Correcting code

--- EasySave_Graphique/EasySave_Graphique/MainWindow.xaml.cs
+++ EasySave_Graphique/EasySave_Graphique/MainWindow.xaml.cs
@@ -25,38 +25,25 @@ namespace EasySave_Graphique
         {
             int index = int.Parse(((Button)e.Source).Uid);
 
-            GridCursor.Margin = new Thickness(10 + (150 * index), 0, 0, 0);
-
             switch (index)
             {
                 case 0:
-                    GridMain.Background = Brushes.Aquamarine;
                     break;
                 case 1:
-                    GridMain.Background = Brushes.Beige;
                     break;
                 case 2:
-                    GridMain.Background = Brushes.CadetBlue;
                     break;
                 case 3:
-                    GridMain.Background = Brushes.DarkBlue;
                     break;
                 case 4:
-                    GridMain.Background = Brushes.Firebrick;
                     break;
                 case 5:
-                    GridMain.Background = Brushes.Gainsboro;
                     break;
                 case 6:
-                    GridMain.Background = Brushes.HotPink;
                     break;
             }
         }
 
-        private void Button_Click_1(object sender, RoutedEventArgs e)
-        {
-
-        }
 
         private void Execute_Button_Click(object sender, RoutedEventArgs e)
         {
@@ -64,25 +51,10 @@ namespace EasySave_Graphique
             Main.Content = new Saves();
         }
 
-        private void Edit_Button_Click(object sender, RoutedEventArgs e)
-        {
-            GridMain.Background = Brushes.Beige;
-        }
-
         private void Logs_Button_Click(object sender, RoutedEventArgs e)
         {
             while (Main.NavigationService.RemoveBackEntry() != null) ;
             Main.Content = new Logs();
-        }
-
-        private void Settings_Button_Click(object sender, RoutedEventArgs e)
-        {
-            GridMain.Background = Brushes.DarkBlue;
-        }
-
-        private void Button_Click_2(object sender, RoutedEventArgs e)
-        {
-
         }
     }
 }

f46a2b146c21c9ceb0242ef9fdffcda2ec712ac4
Louis DUMONT <louis.dumont@viacesi.fr>
2022-12-07 13:56:09 +0100

    Managing language at app start

--- EasySave_WPF/EasySave_WPF/Views/SelectLanguage.xaml.cs
+++ EasySave_WPF/EasySave_WPF/Views/SelectLanguage.xaml.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using EasySave_WPF.Controllers;
+using EasySave_WPF.Models;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Text;
+using System.Threading;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -17,9 +21,39 @@ namespace EasySave_WPF.Views
     /// </summary>
     public partial class SelectLanguage : Window
     {
-        public SelectLanguage()
+        bool quit_program = true;
+        public SelectLanguage(Settings settings)
         {
             InitializeComponent();
+
+            var cbItems = new ObservableCollection<ComboBoxItem>();
+            foreach (string language in settings.AvailableLanguage)
+            {
+                if (language == "fr")
+                    cbItems.Add(new ComboBoxItem { Content = "Français", Tag = language });
+                if (language == "en")
+                    cbItems.Add(new ComboBoxItem { Content = "English", Tag = language });
+            }
+                
+            LanguageComboBox.SelectedIndex = 0;
+            LanguageComboBox.ItemsSource = cbItems;
+        }
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (quit_program)
+            {
+                Environment.Exit(0);
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            ComboBoxItem typeItem = (ComboBoxItem)LanguageComboBox.SelectedItem;
+            string language = typeItem.Tag.ToString();
+            new LanguageController().DefineLanguage(language);
+
+            quit_program = false;
+            Close();
         }
     }
 }

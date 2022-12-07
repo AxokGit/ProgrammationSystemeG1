fb3d469b58945c620cbf7fab2cd809ad685d0d80
Maxime EY <maxime.ey@viacesi.fr>
2022-12-07 14:35:23 +0100

    Work in progress

--- EasySave_WPF/EasySave_WPF/Views/MainWindow.xaml.cs
+++ EasySave_WPF/EasySave_WPF/Views/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using EasySave_WPF.Views;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading;
@@ -35,6 +36,27 @@ namespace EasySave_WPF
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             MyLabel.Content = Convert.ToString(Convert.ToInt32(MyLabel.Content) + 1);
+        }
+
+        public interface IDynamicGridViewModel
+        {
+            ObservableCollection<ObservableCollection<ICellViewModel>> Cells { get; }
+
+            int GridHeight { get; }
+        }
+
+        public interface ICellViewModel
+        {
+            ICell Cell { get; set; }
+            ICommand ChangeCellStateCommand { get; }
+        }
+
+        public interface ICell
+        {
+            /// <summary>
+            /// State of the cell.
+            /// </summary>
+            bool State { get; set; }
         }
     }
 }
